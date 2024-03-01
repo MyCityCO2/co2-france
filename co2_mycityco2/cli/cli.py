157d00af0caf8f5d9b0fdf358fe5845de0488683
Adam Bonnet <abo@open-net.ch>
2024-03-01 15:39:16 +0100

    chore(cli): removing unwanted cli command

--- co2_mycityco2/cli/cli.py
+++ co2_mycityco2/cli/cli.py
@@ -9,7 +9,6 @@ from .format import cli as format_cli
 
 cli = typer.Typer(no_args_is_help=True)
 
-cli.add_typer(format_cli, name="format")
 
 logger.remove(0)
 logger.level("FTRACE", no=3, color="<blue>")
@@ -24,12 +23,4 @@ logger.add(
     level=settings.LOGURU_LEVEL,
 )
 
-
-@cli.command()
-def hello():
-    logger.info("Hello from mycityco2 plugins")
-
-
-@cli.command()
-def goodbye():
-    logger.info("Goodbye from mycityco2 plugins")
+cli.add_typer(format_cli, name="format")
